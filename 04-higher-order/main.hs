51d018bff93e66eea04cb6a25755a960e737ea59
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2015-07-13 17:44:50 +0300

    do week 4 lecture, write homework 4 exercise 1

---
+++ 04-higher-order/main.hs
@@ -0,0 +1,23 @@
+module Main where
+
+main :: IO ()
+main = putStrLn "hello, world"
+
+fun1 :: [Integer] -> Integer
+fun1 [] = 1
+fun1 (x:xs)
+     | even x    = (x - 2) * fun1 xs
+     | otherwise = fun1 xs
+
+fun1' :: [Integer] -> Integer
+fun1' xs = product $ map (subtract 2) $ filter even xs
+
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 n | even n = n + fun2 (n `div` 2)
+       | otherwise = fun2 (3 * n + 1)
+
+fun2' :: Integer -> Integer
+fun2' n = sum $ filter even $ takeWhile (/= 1)
+              $ iterate (\x -> if even x then x `div` 2 else 3 * x + 1) n
+

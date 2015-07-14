73a8e11609d39d0cebacea436648c919a6c51406
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2015-07-14 03:26:07 +0300

    write homework 5, exercise 3, 4

--- 05-type-classes/Calc.hs
+++ 05-type-classes/Calc.hs
@@ -3,13 +3,56 @@ module Calc where
 import ExprT
 import Parser
 
+class Expr e where
+  lit :: Integer -> e
+  add :: e -> e -> e
+  mul :: e -> e -> e
+
+
 eval :: ExprT -> Integer
 eval (Lit i) = i
 eval (Add e f) = eval e + eval f
 eval (Mul e f) = eval e * eval f
 
+instance Expr ExprT where
+  lit = Lit
+  add = Add
+  mul = Mul
+
 evalStr :: String -> Maybe Integer
 evalStr s = maybe Nothing (Just . eval) $ parseExp Lit Add Mul s
             -- compiler warnings suggest using <$> above, but I don't really
             -- know about that yet
+
+
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*)
+
+
+instance Expr Bool where
+  lit = (> 0)
+  add = (||)
+  mul = (&&)
+
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+  lit = MinMax
+  add (MinMax m) (MinMax n) = MinMax (max m n)
+  mul (MinMax m) (MinMax n) = MinMax (min m n)
+
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr Mod7 where
+  lit = Mod7
+  add (Mod7 m) (Mod7 n) = Mod7 ((m + n) `mod` 7)
+  mul (Mod7 m) (Mod7 n) = Mod7 ((m * n) `mod` 7)
+
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
 

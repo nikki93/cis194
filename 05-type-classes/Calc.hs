8a20a8c6e411385de9cd465d29919d34120f71e2
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2015-07-14 04:01:53 +0300

    do homework 5, exercise 5

--- 05-type-classes/Calc.hs
+++ 05-type-classes/Calc.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+
 module Calc where
 
 import ExprT
 import Parser
+import StackVM
 
 class Expr e where
   lit :: Integer -> e
@@ -11,16 +15,16 @@ class Expr e where
 
 eval :: ExprT -> Integer
 eval (Lit i) = i
-eval (Add e f) = eval e + eval f
-eval (Mul e f) = eval e * eval f
+eval (ExprT.Add e f) = eval e + eval f
+eval (ExprT.Mul e f) = eval e * eval f
 
 instance Expr ExprT where
   lit = Lit
-  add = Add
-  mul = Mul
+  add = ExprT.Add
+  mul = ExprT.Mul
 
 evalStr :: String -> Maybe Integer
-evalStr s = maybe Nothing (Just . eval) $ parseExp Lit Add Mul s
+evalStr s = maybe Nothing (Just . eval) $ parseExp Lit ExprT.Add ExprT.Mul s
             -- compiler warnings suggest using <$> above, but I don't really
             -- know about that yet
 
@@ -55,4 +59,13 @@ instance Expr Mod7 where
 
 testExp :: Expr a => Maybe a
 testExp = parseExp lit add mul "(3 * -4) + 5"
+
+
+instance Expr Program where
+  lit = (: []) . PushI
+  add = ((++ [StackVM.Add]) .) . (++)
+  mul = ((++ [StackVM.Mul]) .) . (++)
+
+compile :: String -> Maybe Program
+compile = parseExp lit add mul
 

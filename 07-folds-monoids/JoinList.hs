86273d1936fa3a1948b9eb76bea345641a709650
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2015-07-15 06:13:00 +0300

    do homework 7 exercise 2

--- 07-folds-monoids/JoinList.hs
+++ 07-folds-monoids/JoinList.hs
@@ -2,6 +2,8 @@ module JoinList where
 
 import Data.Monoid
 
+import Sized
+
 data JoinList m a = Empty
                   | Single m a
                   | Append m (JoinList m a) (JoinList m a)
@@ -16,4 +18,29 @@ tag (Append m _ _) = m
 j +++ Empty = j
 Empty +++ j = j
 j +++ k = Append (tag j <> tag k) j k
+
+indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
+indexJ _ Empty = Nothing
+indexJ 0 (Single _ e) = Just e
+indexJ _ (Single _ _) = Nothing
+indexJ i (Append _ j k) | i < sj = indexJ i j
+                        | otherwise = indexJ (i - sj) k
+                        where sj = getSize (size $ tag j)
+
+dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+dropJ n j | n <= 0 = j
+dropJ _ Empty = Empty
+dropJ _ (Single _ _) = Empty -- n <= 0 case handled above, so we have n > 0
+dropJ n (Append _ j k) | n < sj = dropJ n j +++ k
+                       | otherwise = dropJ (n - sj) k
+                       where sj = getSize (size $ tag j)
+
+takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+takeJ n _ | n <= 0 = Empty
+takeJ _ Empty = Empty
+takeJ n j | n >= getSize (size $ tag j) = j
+takeJ n (Append _ j k) | n < sj = takeJ n j
+                       | otherwise = j +++ takeJ (n - sj) k
+                       where sj = getSize (size $ tag j)
+
 

8a7bb5f535c1ca1328136599d0d530652c90722b
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2015-07-15 05:16:32 +0300

    do lecture 7, homework 7 exercise 1

---
+++ 07-folds-monoids/JoinList.hs
@@ -0,0 +1,19 @@
+module JoinList where
+
+import Data.Monoid
+
+data JoinList m a = Empty
+                  | Single m a
+                  | Append m (JoinList m a) (JoinList m a)
+                  deriving (Eq, Show)
+
+tag :: Monoid m => JoinList m a -> m
+tag Empty = mempty
+tag (Single m _) = m
+tag (Append m _ _) = m
+
+(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+j +++ Empty = j
+Empty +++ j = j
+j +++ k = Append (tag j <> tag k) j k
+
